e6661431a046f9f9eb114e38b2c1b0451b58e5ba
mehedihasanmondol <mehedihasanmondol.online@gmail.com>
2025-12-02 16:45:41 +0600

    artisan command executor added

--- app/Http/Controllers/Admin/SystemSettingsController.php
+++ app/Http/Controllers/Admin/SystemSettingsController.php
@@ -20,6 +20,12 @@ class SystemSettingsController extends Controller
                 'icon' => 'M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15',
                 'component' => 'cache-management',
             ],
+            'artisan' => [
+                'title' => 'Artisan Commands',
+                'description' => 'Execute Laravel artisan commands',
+                'icon' => 'M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z',
+                'component' => 'artisan-command-runner',
+            ],
             'maintenance' => [
                 'title' => 'Construction Mode',
                 'description' => 'Enable/disable site maintenance mode',
