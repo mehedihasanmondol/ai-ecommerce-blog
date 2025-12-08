b70e5d1a07b9fa9f09c92f16dcae44702614054a
mehedihasanmondol <mehedihasanmondol.online@gmail.com>
2025-12-08 17:16:18 +0600

    dev bat update

--- start-dev.bat
+++ start-dev.bat
@@ -1,19 +1,19 @@
 @echo off
-set PROJECT_DIR="C:\Users\Love Station\Documents\alom vai\website\ecommerce"
+set PROJECT_DIR=%~dp0
 
 echo Starting Laravel + NPM + Editor + Browser...
 
 :: --- Start PHP Artisan Serve in Git Bash ---
-start "" "C:\Program Files\Git\git-bash.exe" -c "cd %PROJECT_DIR% && php artisan serve; echo '--- PHP SERVER CLOSED ---'; read -p 'Press ENTER to exit...'"
+start "" /D "%PROJECT_DIR%" "C:\Program Files\Git\git-bash.exe" -c "php artisan serve; echo '--- PHP SERVER CLOSED ---'; exec bash"
 
 :: --- Start NPM Run Dev in new Git Bash window ---
-start "" "C:\Program Files\Git\git-bash.exe" -c "cd %PROJECT_DIR% && npm run dev; echo '--- NPM SERVER CLOSED ---'; read -p 'Press ENTER to exit...'"
+start "" /D "%PROJECT_DIR%" "C:\Program Files\Git\git-bash.exe" -c "npm run dev; echo '--- NPM SERVER CLOSED ---'; exec bash"
 
 :: --- Start Git Status in new Git Bash window ---
-start "" "C:\Program Files\Git\git-bash.exe" -c "cd %PROJECT_DIR% && git status; echo '--- GIT STATUS CLOSED ---'; read -p 'Press ENTER to exit...'"
+start "" /D "%PROJECT_DIR%" "C:\Program Files\Git\git-bash.exe" -c "git status; echo '--- GIT STATUS CLOSED ---'; exec bash"
 
 :: --- Open Antigravity Editor ---
-start "" "C:\Users\Love Station\AppData\Local\Programs\Antigravity\antigravity.exe" %PROJECT_DIR%
+start "" "%USERPROFILE%\AppData\Local\Programs\Antigravity\antigravity.exe" "%PROJECT_DIR%"
 
 :: --- Open Browser with Laravel URLs ---
 start "" chrome "http://localhost:8000"
